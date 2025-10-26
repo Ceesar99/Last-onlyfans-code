a76879ff3f38947ce087f9b168d85e1081bae15b
Ceesar99 <dishusamuelprosper@gmail.com>
2025-10-26 19:57:17 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,9 @@ export default defineConfig({
     server: {
         port: 5000,
             host: '0.0.0.0',
-                allowedHosts: [
-                        // ✅ Add your dynamic Replit host here
-                                "1420e70a-9753-42c1-b6d7-4df3cce03a78-00-1l9m6zevytxmu.riker.replit.dev"
-                                      ],
-                                          strictPort: true,
-                                              hmr: {
-                                                    clientPort: 5000
-                                                        }
-                                                          }
-                                                          });
+      strictPort: true,
+         hmr: {
+       clientPort: 5000
+} 
+}
+});
