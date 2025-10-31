497a6c168669b100cfeddd32f69c9d12125e0571
Ceesar99 <dishusamuelprosper@gmail.com>
2025-10-31 22:03:06 +0100

    Update ProfilePage.jsx☺️✅

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef, useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import ModalPortal from "../component/ModalPortal";
-import SubscriptionModal from "../component/SubscriptionModal";
+import SubscriptionModal from "../component/SubcriptionModal";
 // frontend supabase client (assumes default export)
 import supabase from "../supabaseclient";
 
