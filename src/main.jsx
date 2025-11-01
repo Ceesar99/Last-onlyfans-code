2316b9215b2b99c266c2f5beb2f1c4bde23036b7
Ceesar99 <dishusamuelprosper@gmail.com>
2025-11-01 14:23:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+console.log("✅ main.jsx started running");
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
