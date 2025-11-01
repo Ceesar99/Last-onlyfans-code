cc10fd868baba756d4390cbf9a057777d0b2f4f4
Ceesar99 <dishusamuelprosper@gmail.com>
2025-11-01 17:16:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ProfilePage from "./Pages/ProfilePage";
-import { AuthProvider } from "./context/Authcontext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingSplash from "./components/LoadingSplash";
+import { AuthProvider } from "./context/Authcontext";
+import MessagesPage from "./Pages/MessagesPage";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <LoadingSplash>
-        <ProfilePage />
-      </LoadingSplash>
-    </AuthProvider>
+    <LoadingSplash>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MessagesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </LoadingSplash>
   </React.StrictMode>
 );
