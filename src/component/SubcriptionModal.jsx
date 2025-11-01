6964ec88ce1c2b3f3c9dd3f6500052c8b91714d0
Ceesar99 <dishusamuelprosper@gmail.com>
2025-11-01 01:15:57 +0100

    Update SubcriptionModal.jsx

--- src/component/SubcriptionModal.jsx
+++ src/component/SubcriptionModal.jsx
@@ -116,8 +116,8 @@ export default function SubscriptionModal({
             <span className="inline-flex items-center" aria-hidden>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
+                width="34"
+                height="36"
                 viewBox="0 0 33 32"
                 role="img"
                 aria-label="Verified"
