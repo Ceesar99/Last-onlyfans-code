cebaf242ddbaa38cfb426963c8a449d4f4661f94
Ceesar99 <dishusamuelprosper@gmail.com>
2025-11-02 11:08:50 +0100

    Update ProfilePage.jsx

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -2,8 +2,9 @@
 import React, { useEffect, useRef, useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import ModalPortal from "../component/ModalPortal";
-
+import SubscriptionModal from "../component/SubcriptionModal";
 // frontend supabase client (assumes default export)
+import supabase from "../supabaseclient";
 
 const FREE_SAMPLE_LS_KEY = "freeSampleAccess_v1";
 
