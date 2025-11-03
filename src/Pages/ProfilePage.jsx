974a8fd81e3bc919544b25c9c2eb4e8cbaa30ee7
Ceesar99 <dishusamuelprosper@gmail.com>
2025-11-04 00:33:34 +0100

    Update ProfilePage.jsx

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 // TRY IMPORTING ONE AT A TIME - COMMENT OUT THE OTHERS
 import ModalPortal from "../component/ModalPortal";
 import SubscriptionModal from "../component/SubcriptionModal";
-// import AddCardForm from "../component/AddCardForm";
+import AddCardForm from "../component/AddCardForm";
 
 const defaultCreator = {
   name: "Tayler Hills",
