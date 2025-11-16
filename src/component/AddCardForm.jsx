7221b833bd29766b1981e61df11fe948fca2ff25
Ceesar99 <dishusamuelprosper@gmail.com>
2025-11-16 12:18:31 +0100

    Create AddCardForm component

---
+++ src/component/AddCardForm.jsx
@@ -0,0 +1,13 @@
+// AddCardForm.jsx
+import React from 'react';
+
+const AddCardForm = () => {
+    return (
+        <div>
+            <h1>Add a New Card</h1>
+            {/* Form implementation goes here */}
+        </div>
+    );
+};
+
+export default AddCardForm;
